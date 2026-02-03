f6878955e7209bd2b5ebf9b8d061eb922d561f1f
osgial <osgial@alumnatflorida.es>
2026-02-03 12:18:21 +0100

    Sexto commit(cambios roberto)

--- php/index.php
+++ php/index.php
@@ -1,8 +1,8 @@
 <?php
 
 $servername = "db";
-$username = "davici";
-$password = "davici1234";
+$username = "root";
+$password = "pinacoteca1234";
 $dbname = "pinacoteca";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
